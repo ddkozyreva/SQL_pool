41d269e8e88148d5c02bd3dd059bd572952a5069
Loreta Themis <loretath@at-i2.msk.21-school.ru>
2023-02-06 20:15:39 +0300

    6

--- src/ex05/day00_ex05.sql
+++ src/ex05/day00_ex05.sql
@@ -1,26 +1,16 @@
--- select 
---     name 
--- from 
---     person 
--- where 
---     id = ANY
---     (select person_id from person_order 
---     where (menu_id = 13 or menu_id = 14 or menu_id = 18) and
---     order_date = '2022-01-07');
-
 SELECT (
     SELECT 
         name 
-    from 
+    FROM 
         person 
     WHERE 
         person.id = person_order.person_id
-    )     
-from 
+    ) AS name
+FROM 
     person_order 
 WHERE (
-    menu_id = 13 or 
-    menu_id = 14 or 
+    menu_id = 13 OR 
+    menu_id = 14 OR 
     menu_id = 18) 
-    and
+    AND
     order_date = '2022-01-07';
