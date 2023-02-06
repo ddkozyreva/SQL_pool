746772c1aa658b75038cfddbf9e5777b2561f8a7
Loreta Themis <loretath@at-i2.msk.21-school.ru>
2023-02-06 19:00:19 +0300

    5-update

--- src/ex05/day00_ex05.sql
+++ src/ex05/day00_ex05.sql
@@ -1,5 +1,26 @@
-select name from person 
-where id in
-(select distinct person_id from person_order 
-where menu_id in (13,14,18) and
-order_date = '2022-01-07');
+-- select 
+--     name 
+-- from 
+--     person 
+-- where 
+--     id = ANY
+--     (select person_id from person_order 
+--     where (menu_id = 13 or menu_id = 14 or menu_id = 18) and
+--     order_date = '2022-01-07');
+
+SELECT (
+    SELECT 
+        name 
+    from 
+        person 
+    WHERE 
+        person.id = person_order.person_id
+    )     
+from 
+    person_order 
+WHERE (
+    menu_id = 13 or 
+    menu_id = 14 or 
+    menu_id = 18) 
+    and
+    order_date = '2022-01-07';
