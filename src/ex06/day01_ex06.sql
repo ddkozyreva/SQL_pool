f2f7b87b5b4c370e029f85835f4504ab162c5baf
Loreta Themis <loretath@il-b3.msk.21-school.ru>
2023-02-07 20:22:32 +0300

    6 pipyao

---
+++ src/ex06/day01_ex06.sql
@@ -0,0 +1,28 @@
+SELECT
+    order_date AS action_date,
+    (
+        SELECT
+            name AS person_name
+        FROM
+            person
+        WHERE
+            id = person_order.person_id
+    )
+FROM
+    person_order
+INTERSECT
+SELECT
+    visit_date AS action_date,
+    (
+        SELECT
+            name AS person_name
+        FROM
+            person
+        WHERE
+            id = person_visits.person_id
+    )
+FROM
+    person_visits
+ORDER BY
+    action_date,
+    person_name DESC;
