e3f71b73a915b96b63fd14732ff688f8d4de652d
Loreta Themis <loretath@il-b3.msk.21-school.ru>
2023-02-07 20:39:04 +0300

    7

---
+++ src/ex07/day01_ex07.sql
@@ -0,0 +1,9 @@
+SELECT
+    person_order.order_date,
+    concat(person.name, ' (age:', person.age, ')') AS person_information
+FROM
+    person_order
+    INNER JOIN person ON person.id = person_order.person_id
+ORDER BY
+    order_date,
+    person_information;
