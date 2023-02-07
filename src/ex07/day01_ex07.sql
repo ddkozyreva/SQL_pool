4cd735fa1c4f122a6dba79a8c7eb93c3a3e26d00
Loreta Themis <loretath@il-b3.msk.21-school.ru>
2023-02-07 20:56:26 +0300

    8

--- src/ex07/day01_ex07.sql
+++ src/ex07/day01_ex07.sql
@@ -3,7 +3,7 @@ SELECT
     concat(person.name, ' (age:', person.age, ')') AS person_information
 FROM
     person_order
-    INNER JOIN person ON person.id = person_order.person_id
+    JOIN person ON person.id = person_order.person_id
 ORDER BY
     order_date,
     person_information;
