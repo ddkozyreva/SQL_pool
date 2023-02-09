d597fb81358f5190f6ae56397487ac24b0d7d22f
Loreta Themis <loretath@il-b3.msk.21-school.ru>
2023-02-09 15:58:54 +0300

    5

---
+++ src/ex05/day03_ex05.sql
@@ -0,0 +1,33 @@
+WITH visits AS (
+    SELECT
+        piz.name AS pizzeria_name
+    FROM
+        person p
+        JOIN person_visits pv ON pv.person_id = p.id
+        JOIN pizzeria piz ON piz.id = pv.pizzeria_id
+    WHERE
+        p.name = 'Andrey'
+),
+orders AS (
+    SELECT
+        piz.name AS pizzeria_name
+    FROM
+        person p
+        JOIN person_order po ON po.person_id = p.id
+        JOIN menu m ON m.id = po.menu_id
+        JOIN pizzeria piz ON piz.id = m.pizzeria_id
+    WHERE
+        p.name = 'Andrey'
+) (
+    SELECT
+        pizzeria_name
+    FROM
+        visits
+)
+EXCEPT
+    ALL (
+        SELECT
+            pizzeria_name
+        FROM
+            orders
+    )
