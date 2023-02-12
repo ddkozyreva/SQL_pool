ad23be30600a467ba26bae4d066756a3a7cf4a92
Loreta Themis <loretath@mi-c3.msk.21-school.ru>
2023-02-12 15:29:27 +0300

    min_values

--- src/team00_ex00.sql
+++ src/team00_ex00.sql
@@ -1,48 +1,49 @@
-WITH RECURSIVE possible_routes AS (
+WITH RECURSIVE find_path AS (
     SELECT
-        routes.destination,
-        routes.departure || '->' || routes.destination AS tour,
-        routes.cost,
-        routes.destination = 'B' AS b_flag,
-        routes.destination = 'C' AS c_flag,
-        routes.destination = 'D' AS d_flag
+        destination,
+        departure || ',' || destination as route,
+        cost,
+        RIGHT(destination, 1) as history
     FROM
         routes
     WHERE
-        routes.departure = 'A'
+        departure = 'A'
     UNION
-    ALL
     SELECT
         routes.destination,
-        possible_routes.tour || '->' || routes.destination AS tour,
-        possible_routes.cost + routes.cost,
-        -- possible_routes - в итоге это предпоследний пункт
-        -- if b_flag true then b_flag = true else b_flag = (routes.destination = 'B')
-        possible_routes.destination = 'B' AS b_flag,
-        possible_routes.destination = 'C' AS c_flag,
-        possible_routes.destination = 'D' AS d_flag
+        find_path.route || ',' || routes.destination AS route,
+        find_path.cost + routes.cost as cost,
+        RIGHT(find_path.route, 1) as history
     FROM
-        possible_routes
-        INNER JOIN routes ON routes.departure = possible_routes.destination
+        routes
+        JOIN find_path ON find_path.destination = routes.departure
     WHERE
-        possible_routes.cost < 100
+        char_length(find_path.route) < 7
+        AND find_path.history != routes.destination
+        AND routes.destination != 'A'
+),
+final_routes AS (
+    SELECT
+        find_path.cost + routes.cost as total_cost,
+        route || ',A' as tour
+    FROM
+        find_path
+        JOIN routes ON routes.destination = 'A'
+        AND RIGHT(find_path.route, 1) = routes.departure
+    WHERE
+        char_length(find_path.route) = 7
+    ORDER BY
+        total_cost
 )
 SELECT
-    --конечный пункт
-    destination,
-    possible_routes.tour,
-    possible_routes.cost,
-    b_flag,
-    c_flag,
-    d_flag
+    total_cost,
+    tour
 FROM
-    possible_routes
+    final_routes
 WHERE
-    -- b_flag = true
-    -- AND 
-    -- c_flag = true
-    -- AND d_flag = true
-    -- AND 
-    possible_routes.destination = 'A'
-ORDER BY
-    possible_routes.cost;
+    total_cost IN (
+        SELECT
+            min(total_cost)
+        FROM
+            final_routes
+    );
