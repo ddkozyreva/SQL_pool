3809251efe9e8d9a845a9bd548cfbc6aeb57351c
Loreta Themis <loretath@il-b3.msk.21-school.ru>
2023-02-14 20:35:34 +0300

    fix

--- src/ex00/day05_ex00.sql
+++ src/ex00/day05_ex00.sql
@@ -1,9 +1,9 @@
-CREATE INDEX idx_menu_pizzeria_id ON pizzeria(id);
+CREATE INDEX idx_menu_pizzeria_id ON menu(pizzeria_id);
 
-CREATE INDEX idx_person_order_person_id ON person(id);
+CREATE INDEX idx_person_order_person_id ON person_order(person_id);
 
-CREATE INDEX idx_person_order_menu_id ON menu(id);
+CREATE INDEX idx_person_order_menu_id ON person_order(menu_id);
 
-CREATE INDEX idx_person_visits_person_id ON person(id);
+CREATE INDEX idx_person_visits_person_id ON person_visits(person_id);
 
-CREATE INDEX idx_person_visits_pizzeria_id ON pizzeria(id);
+CREATE INDEX idx_person_visits_pizzeria_id ON person_visits(pizzeria_id);
--- src/ex01/day05_ex01.sql
+++ src/ex01/day05_ex01.sql
@@ -3,8 +3,8 @@ SET
 
 EXPLAIN ANALYZE
 SELECT
-    m.pizza_name,
-    piz.name
+    m.pizza_name AS pizza_name,
+    piz.name AS pizzeria_name
 FROM
     menu m
     JOIN pizzeria piz ON m.pizzeria_id = piz.id;
