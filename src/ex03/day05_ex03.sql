35abb1915822770701f7fc40370927146a5e4eaf
Loreta Themis <loretath@il-b3.msk.21-school.ru>
2023-02-14 16:02:38 +0300

    fix 3

--- src/ex03/day05_ex03.sql
+++ src/ex03/day05_ex03.sql
@@ -1,4 +1,4 @@
-CREATE INDEX idx_person_order_multi ON person_order(person_id, menu_id);
+CREATE INDEX idx_person_order_multi ON person_order(person_id, menu_id, order_date);
 
 SET
     enable_seqscan = OFF;
