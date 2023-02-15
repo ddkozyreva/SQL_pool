681120783b833940b884efe81cd64ff0698c3af7
Loreta Themis <loretath@il-b3.msk.21-school.ru>
2023-02-15 22:30:49 +0300

    9

--- src/ex06/day07_ex06.sql
+++ src/ex06/day07_ex06.sql
@@ -1,7 +1,7 @@
 SELECT
     pz.name,
     count(po.order_date) AS count_of_orders,
-    round(avg(price), 2),
+    round(avg(price), 2) AS average_price,
     max(price) AS max_price,
     min(price) AS min_price
 FROM
