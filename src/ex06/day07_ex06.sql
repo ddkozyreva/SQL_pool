98d784985de7ec0aef531019872984d7ebf10a54
Loreta Themis <loretath@il-b3.msk.21-school.ru>
2023-02-15 21:39:19 +0300

    6

---
+++ src/ex06/day07_ex06.sql
@@ -0,0 +1,14 @@
+SELECT
+    pz.name,
+    count(po.order_date) AS count_of_orders,
+    round(avg(price), 2),
+    max(price) AS max_price,
+    min(price) AS min_price
+FROM
+    pizzeria pz
+    JOIN menu m ON m.pizzeria_id = pz.id
+    JOIN person_order po ON po.menu_id = m.id
+GROUP BY
+    pz.name
+ORDER BY
+    pz.name;
