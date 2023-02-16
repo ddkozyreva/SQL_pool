7c0b9058b8dc6656e6c6520effa44bb173acaf98
Loreta Themis <loretath@oa-o2.msk.21-school.ru>
2023-02-16 15:45:14 +0300

    1

---
+++ src/ex01/day08_ex01.sql
@@ -0,0 +1,37 @@
+-- Session 1
+begin;
+
+select
+    rating
+from
+    pizzeria
+where
+    name = 'Pizza Hut';
+
+update
+    pizzeria
+set
+    rating = 4
+WHERE
+    name = 'Pizza Hut';
+
+commit;
+
+-- Session 2
+begin;
+
+select
+    rating
+from
+    pizzeria
+where
+    name = 'Pizza Hut';
+
+update
+    pizzeria
+set
+    rating = 3.6
+WHERE
+    name = 'Pizza Hut';
+
+commit;
