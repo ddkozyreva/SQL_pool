f160baac4c35e9fbcd200006f4af217c3560b078
Loreta Themis <loretath@il-b3.msk.21-school.ru>
2023-02-18 17:31:43 +0300

    fix 4

--- src/ex04/day09_ex04.sql
+++ src/ex04/day09_ex04.sql
@@ -1,7 +1,14 @@
 create function fnc_persons_female() returns 
 table (id bigint, name varchar, age int, gender varchar, address varchar) 
 AS $$
-    select * from person;
+    select * from person where gender = 'female';
+$$ language sql;
+
+create function fnc_persons_male() returns 
+table (id bigint, name varchar, age int, gender varchar, address varchar) 
+AS $$
+    select * from person where gender = 'male';
 $$ language sql;
 
 select * from fnc_persons_female();
+select * from fnc_persons_male();
