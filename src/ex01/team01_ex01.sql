07d773b43be7792b1238add9910e0a0072540056
Knew1t <longshot@mi-a6.msk.21-school.ru>
2023-02-19 21:35:19 +0300

    fml

--- src/ex01/team01_ex01.sql
+++ src/ex01/team01_ex01.sql
@@ -1,10 +1,11 @@
 -- insert into currency values (100, 'EUR', 0.85, '2022-01-01 13:29');
 -- insert into currency values (100, 'EUR', 0.79, '2022-01-08 13:29');
 
-create or replace function get_rate(b_id int, b_date timestamp without time zone) returns setof numeric as $$
+create or replace function get_rate(b_id int, b_date timestamp without time zone) returns numeric as $$
+declare
+    min_date_rate numeric;
+    max_date_rate numeric;
 begin
-    return query
-( 
     select 
         rate_to_usd
     from
@@ -16,9 +17,7 @@ begin
         c.updated < b_date
     order by c.updated desc
     limit 1
- ) 
-    union
-( 
+    into min_date_rate;
     select 
         rate_to_usd
     from
@@ -27,27 +26,27 @@ begin
         and
         c.id = b_id
     where 
-        c.updated > b_date
-    order by c.updated asc
+        c.updated >= b_date
+    order by c.updated desc
     limit 1
- )
- limit 1;
+    into max_date_rate;
+    return coalesce(min_date_rate, max_date_rate);
 end; 
 $$ language plpgsql;
 
-select
+select distinct
     coalesce(u.name, 'not defined') as name,
     coalesce(u.lastname, 'not defined') as lastname,
     c .name as currency_name,
-    get_rate(b.currency_id, b.updated) * b.money as currency_in_usd,
+    get_rate(b.currency_id, b.updated) * b.money as currency_in_usd
     -- b.currency_id,
-    c.rate_to_usd,
-    b.updated as b_date,
-    c.updated as old_update
+    -- c.rate_to_usd,
+    -- b.updated as b_date,
+    -- c.updated as old_update
 from
     balance b
     left join public .user u on u.id = b.user_id
     join currency c on b.currency_id = c .id 
     and c .id is not null
 order by
-    1 desc, 2 asc, 3 asc
+    1 desc, 2 asc, 3 asc;
