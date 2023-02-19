4dda09c211e3ed15c1008af1e9f309a62cef7b5a
Knew1t <longshot@mi-a6.msk.21-school.ru>
2023-02-19 22:14:27 +0300

    ff

--- src/ex01/team01_ex01.sql
+++ src/ex01/team01_ex01.sql
@@ -1,5 +1,5 @@
--- insert into currency values (100, 'EUR', 0.85, '2022-01-01 13:29');
--- insert into currency values (100, 'EUR', 0.79, '2022-01-08 13:29');
+-- -- insert into currency values (100, 'EUR', 0.85, '2022-01-01 13:29');
+-- -- insert into currency values (100, 'EUR', 0.79, '2022-01-08 13:29');
 
 create or replace function get_rate(b_id int, b_date timestamp without time zone) returns numeric as $$
 declare
@@ -34,19 +34,54 @@ begin
 end; 
 $$ language plpgsql;
 
-select distinct
+create or replace function get_date(b_id int, b_date timestamp without time zone) returns timestamp without time zone as $$
+declare
+    past_date timestamp without time zone;
+    future_date timestamp without time zone;
+begin
+    select 
+        c.updated
+    from
+        currency c 
+        join balance b on b_id = b.currency_id 
+        and
+        c.id = b_id
+    where 
+        c.updated < b_date
+    order by c.updated desc
+    limit 1
+    into past_date;
+    select 
+        c.updated
+    from
+        currency c 
+        join balance b on b_id = b.currency_id 
+        and
+        c.id = b_id
+    where 
+        c.updated >= b_date
+    order by c.updated asc
+    limit 1
+    into future_date;
+    return coalesce(past_date, future_date);
+end; 
+$$ language plpgsql;
+
+with tmp as (select
     coalesce(u.name, 'not defined') as name,
     coalesce(u.lastname, 'not defined') as lastname,
-    c .name as currency_name,
-    get_rate(b.currency_id, b.updated) * b.money as currency_in_usd
-    -- b.currency_id,
-    -- c.rate_to_usd,
-    -- b.updated as b_date,
-    -- c.updated as old_update
+    b.currency_id,
+    get_rate(b.currency_id, b.updated) * b.money as currency_in_usd,
+    get_rate(b.currency_id, b.updated) as rate,
+    get_date(b.currency_id, b.updated) as date
+    
 from
     balance b
-    left join public .user u on u.id = b.user_id
-    join currency c on b.currency_id = c .id 
-    and c .id is not null
+    left join public .user u on u.id = b.user_id)
+
+select 
+    tmp.name, tmp.lastname, c.name as currency_name, currency_in_usd  from tmp
+    join currency  c on tmp.currency_id = c .id 
+    and c .id is not null and tmp.date = c.updated
 order by
     1 desc, 2 asc, 3 asc;
