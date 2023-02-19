d8cc9caa4b591211f7e72083ed3921d79f63c2bc
Knew1t <longshot@mi-a6.msk.21-school.ru>
2023-02-19 15:10:27 +0300

    param

--- src/ex00/team01_ex00.sql
+++ src/ex00/team01_ex00.sql
@@ -1,3 +1,21 @@
-select * from balance; 
-select * from currency;
-select * from "user";
+select
+    case
+        when u.name is null then 'not defined'
+        else u.name
+    end name,
+    case
+        when u.lastname is null then 'not defined'
+        else u.lastname
+    end lastname,
+    b.type,
+    b.money,
+    b.currency_id,
+    case
+        when c.name is null then 'not defined'
+        else c.name
+    end currency_name,
+    c.rate_to_usd
+from
+    balance b
+    left join public .user u on u.id = b.user_id
+    left join currency c on c.id = b.currency_id
