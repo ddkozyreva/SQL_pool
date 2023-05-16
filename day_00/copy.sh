5db7041b085d8f6c31029e0e6c111eab1b4e59f9
ddkozyreva <ddkozyreva@edu.hse.ru>
2023-05-16 18:20:02 +0300

    init

---
+++ day_00/copy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+dest="day_00/"
+mkdir $dest
+mv CHANGELOG $dest
+mv README.md $dest
+mv copy.sh $dest
+mv datasets $dest
+mv materials $dest
+mv LICENSE $dest
+mv code-samples $dest
+mv data-samples $dest
+mv misc $dest
+mv src $dest
